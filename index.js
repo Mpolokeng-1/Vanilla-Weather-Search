abb97e3c40b54fb591394a0f70030c1b6c1bc0f3
Mpolokeng-1 <mlsehloho7@gmail.com>
2024-09-05 19:14:22 +0200

    Injected real live content using APIs.

--- index.js
+++ index.js
@@ -53,6 +53,13 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "109284418326b17o4fte0a0fcf58d4d6";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -62,25 +69,30 @@ function getForecast(city) {
 function displayForecast(response) {
   console.log(response.data);
 
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
     <div class="weather-forecast">
     <div class="weather-forecast-day">
-    <div class="weather-forecast-date">${day}</div>
-    <div class="weather-forecast-icon">🌤️</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
+    
+    <img src="${day.condition.icon_url}" class="weather-forecast-icon"/>
+   
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
-        <strong>18°C </strong>
+        <strong>${Math.round(day.temperature.maximum)}°C </strong>
         </div>
-        <div class="weather-forecast-temperature">12°C</div>
+        <div class="weather-forecast-temperature">${Math.round(
+          day.temperature.minimum
+        )}°C</div>
           </div>
           </div>
         `;
+    }
   });
   let forecastElement = document.querySelector("#forecast");
   forecastElement.innerHTML = forecastHtml;
